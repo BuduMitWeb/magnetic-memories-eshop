c702f38fb6853daa4bbaa05c52bedda7932fb83b
magneticmemoriescz-bit <magnetic.memories.cz@gmail.com>
2026-05-17 21:37:32 +0200

    Update FileUpload.tsx

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -96,7 +96,7 @@ export const FileUpload: React.FC<FileUploadProps> = ({ onUploadComplete, requir
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
               </svg>
               <p className="mb-1 text-[13px] text-gray-800 font-bold">Vyberte nebo přetáhněte fotky</p>
-              <p className="text-[9px] text-gray-400 uppercase font-bold tracking-[0.1em]">JPG, PNG nebo WEBP</p>
+              <p className="text-[9px] text-black uppercase font-light tracking-[0.1em]">JPG, PNG nebo WEBP</p>
             </>
           )}
         </div>
